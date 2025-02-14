7c9c10ffb0828549a54e76b2f532f75d19d7de78
rfranr <rfranr@gmail.com>
2025-02-15 00:45:00 +0100

    wichkey
    move

---
+++ .config/nvim/lua/config/plugins/wich-key.lua
@@ -0,0 +1,8 @@
+return {
+    "folke/which-key.nvim",
+    dependencies = {
+      { 'echasnovski/mini.nvim', version = false },
+      { "nvim-tree/nvim-web-devicons", opts = {} },
+    },
+    lazy = true,
+}
